7e6887d5d894e113681f73bdc35b9ebdb3f20f44
gbowers <bowersg45@gmail.com>
2022-08-28 15:12:14 -0400

    Setup port to be 443

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from src import create_app  # Import method to start application
 app = create_app(True)  # Create instance of application
 
 if __name__ == '__main__':  # If this is the main entry point...
-    app.run(port=9000)  # Run the application
+    app.run(port=443)  # Run the application
